b43af1923b6d2fbd0bfe5ec815b12e5bb3b0f10c
Syzymon <syzymoncode@gmail.com>
2024-12-12 14:08:57 +0100

    Import Police

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="font-righteous">HELLO WORLD</h1>
     </div>
   );
 }
