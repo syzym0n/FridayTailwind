13e35967ef875353e587fbfb43b2e9eeb4cc8f76
Syzymon <syzymoncode@gmail.com>
2024-12-13 16:12:07 +0100

    Fonction map sur la data récupérée puis mise en props dans IllustrationCard

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,14 +2,24 @@
 
 import { useState, useEffect } from 'react';
 
-export default function Home() {
+import IllustrationCard from '@/components/beneRohlmann/illustrationCard';
 
-    const [BRData, setBRData] = useState(null);
+interface BRDataItem {
+    id: number;
+    title: string;
+    color: string;
+    pic: string;
+}
+
+export default function Home() {
+    
+
+    const [BRData, setBRData] = useState<BRDataItem[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/BeneRohlmann/data/bene.json'); // Chemin relatif au dossier public
+                const response = await fetch('/BeneRohlmann/data/bene.json')
                 if (!response.ok) {
                     throw new Error('Erreur lors de la récupération des données');
                 }
@@ -27,11 +37,17 @@ export default function Home() {
 
     return (
         <div className="relative z-10">
-            <div>
+            <div className='flex gap-5'>
                 {BRData ? (
-                    <pre>{JSON.stringify(BRData, null, 2)}</pre>
+                    BRData.map((item, id) => (
+                        <IllustrationCard
+                            key={item.id}
+                            color={item.color}
+                            picture={item.pic}
+                        />
+                    ))
                 ) : (
-                    <p>Chargement des données...</p>
+                    <p>Loading</p>
                 )}
             </div>
         </div>
